b9cd15b8a5f3a5c08d9ef926bc58de27055a9aa6
weakbody <cmp8448@uncw.edu>
2013-10-17 11:36:18 -0400

    Code Is broken up a bit.

---
+++ LeapDrone/LeapDrone/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(9, 1);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(34, 32);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Number of fingers: ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(590, 1);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(156, 32);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "Swipe action";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1208, 517);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 47);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Land";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1208, 453);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 47);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "TakeOff";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1208, 390);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 47);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Flat Trim";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Width,
+                                                      System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Height);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ LeapDrone/LeapDrone/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Leap;
+using LeapDrone;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private static Controller _controller;
+        private long _currentTime;
+        private long _previousTime;
+        private long _timeChange;
+        private bool _cursourEnabled;
+        private WinFormsListener _listener;
+        private object sync = new object();
+        AR_Drone ardrone = new AR_Drone();
+
+        public Form1()
+        {
+            InitializeComponent();
+            _listener = new WinFormsListener();
+            _controller = new Controller();
+            if (_controller.IsConnected)
+            {
+                Console.WriteLine("Success");
+            }
+            _controller.AddListener(_listener);
+            _listener.Frame += ListenerOnFrame;
+            _cursourEnabled = false;
+            FormClosed += OnFormClosed;
+        }
+
+        private void OnFormClosed(object sender, FormClosedEventArgs formClosedEventArgs)
+        {
+            _controller.RemoveListener(_listener);
+            _controller.Dispose();
+        }
+
+        private void ListenerOnFrame(Controller controller)
+        {
+            lock (sync)
+            {
+                var fingers = new FingerList();
+                var frame = controller.Frame();
+                _currentTime = frame.Timestamp;
+                _timeChange = _currentTime - _previousTime;
+
+                #region Read frame data
+                if (!frame.Hands.IsEmpty)
+                {
+                    Hand hand = frame.Hands[0];
+                    fingers = hand.Fingers;
+                    if (_timeChange > 1000)
+                    {
+                        if (!fingers.IsEmpty)
+                        {
+                            if (textBox1.InvokeRequired)
+                            {
+                                BeginInvoke(new Action<object>(Update), new object[] { fingers });
+                            }
+                        }
+                    }
+                }
+                #endregion
+                #region Gesture
+                
+                ardrone.Init_AR_Drone();
+                int success = 0;
+
+                GestureList gestures = frame.Gestures();
+                foreach (Gesture gesture in gestures)
+                {
+                    switch (gesture.Type)
+                    {
+                        case Gesture.GestureType.TYPECIRCLE:
+                            success = ardrone.Roll_AR_Drone();
+                            PrintData("Circle gesture");
+                            break;
+                        case Gesture.GestureType.TYPESWIPE:
+                            PrintData("Swipe gesture");
+                            break;
+                        case Gesture.GestureType.TYPEKEYTAP:
+                            PrintData("Tap gesture");
+                            break;
+                        case Gesture.GestureType.TYPESCREENTAP:
+                            PrintData("Tap ScreenTapGesture");
+                            break;
+                        default:
+                            PrintData("Unknown gesture type.");
+                            break;
+                    }
+                }
+                #endregion
+                //#region PalmPosition
+                HandList h = new HandList();
+                var leftMost = h.Leftmost;
+                var rightMost = h.Rightmost;
+                var numberOfHands = h.Count;
+                Hand handy = frame.Hands[0];
+                var pitch = handy.StabilizedPalmPosition.Pitch;
+                var roll = handy.StabilizedPalmPosition.Roll;
+                var xPositionofPalm = handy.PalmPosition.x;
+                var yPositionofPalm = handy.PalmPosition.y;
+                var yaw = handy.StabilizedPalmPosition.Yaw;
+                Console.WriteLine("x: %f y: %f", xPositionofPalm, yPositionofPalm);
+               float changeXInHand, changeYInHand;
+                changeXInHand = xPositionofPalm - handy.StabilizedPalmPosition.x;
+                changeYInHand = yPositionofPalm - handy.StabilizedPalmPosition.y;
+                ardrone.set_X_Y(changeXInHand, changeYInHand);
+                ardrone.setPitch(pitch);
+                ardrone.setYaw(yaw);
+                ardrone.set_Roll(roll);
+                //#endregion
+                #region Mouse
+
+                if (_cursourEnabled)
+                {
+                   
+                    var screen = controller.LocatedScreens.ClosestScreenHit(fingers[0]);
+
+                    if (fingers.Count == 2)
+                    {
+                        if (_timeChange > 4000)
+                        {
+                            var x = (int)screen.Intersect(fingers[0], true, 1.0F).x;
+                            var y = (int)screen.Intersect(fingers[0], true, 1.0F).y;
+                            MouseCursor.mouse_event(0x0002 | 0x0004, 0, x, y, 0);
+                        }
+                    }
+
+                    if (screen != null && screen.IsValid)
+                    {
+                        if ((int)fingers[0].TipVelocity.Magnitude > 25)
+                        {
+                            var xScreenIntersect = screen.Intersect(fingers[0], true).x;
+                            var yScreenIntersect = screen.Intersect(fingers[0], true).y;
+
+                            if (xScreenIntersect.ToString() != "NaN")
+                            {
+                                var x = (int)(xScreenIntersect * screen.WidthPixels);
+                                var y = (int)(screen.HeightPixels - (yScreenIntersect * screen.HeightPixels));
+                                MouseCursor.MoveCursor(x, y);
+                            }
+
+                        }
+                    }
+                }
+
+                #endregion
+                _previousTime = _currentTime;
+                frame.Dispose();
+            }
+        }
+
+        void PrintData(string str)
+        {
+            if (textBox2.InvokeRequired)
+            {
+                BeginInvoke(new Action<string>(UpdateGestureBox), str);
+            }
+        }
+
+        private void Update(object obj)
+        {
+            var fngr = (FingerList)obj;
+            Debug.WriteLine(fngr.Count.ToString());
+            var xRaw = (decimal)fngr[0].TipPosition.x;
+            textBox1.Text = fngr.Count.ToString();
+            textBox1.Location = new Point(
+                CoordinateConverter.TranslateToLeft(xRaw, 1500),
+                700 - (int)fngr[0].TipPosition.y);
+        }
+
+        private void UpdateGestureBox(string str)
+        {
+            Debug.WriteLine(str);
+            textBox2.Text = str;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _cursourEnabled = true;
+            ardrone.AR_Drone_Land();
+            //code to auto land ardrone
+        }
+
+        public void button2_Click(object sender, EventArgs e)
+        {
+            //code to take off and hover
+            ardrone.AR_Drone_Takeoff();
+        }
+
+        public void button3_Click(object sender, EventArgs e)
+        {
+            //code to flat trim
+            ardrone.AR_Drone_FlatTrim();
+        }
+    }
+
+    class MouseCursor
+    {
+        [DllImport("user32.dll")]
+        private static extern bool SetCursorPos(int x, int y);
+
+        public static void MoveCursor(int x, int y)
+        {
+            SetCursorPos(x, y);
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+    }
+}
